c63b0e10c2b528aab506ade0f51ccfbce6552ac5
Alexander <alexitorjlanus@gmail.com>
2023-11-11 20:15:31 -0300

    cambios

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 let express=require("express")
 let app=express()
 let path = require("path")
-
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname,"/public")))
 app.get ( "/", (req,res)=> {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"views/login.html"))
-})
+}) 
+
+app.post("/login", (req,res) => {
+    console.log (req.body);
+    res.redirect("/");
+});
+
+app.post("/register", (req,res) => {
+    console.log (req.body);
+    res.redirect("/");
+});
+
+
+
+
 app.listen(3000,()=>console.log("servidor levantado"))
